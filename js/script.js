fccbe21d439341ca05c1c3bd4a37d81001076ab6
Liza Vasilenko <liza.vasilenko13@gmail.com>
2022-07-02 17:45:58 +0200

    added dark theme to css

--- js/script.js
+++ js/script.js
@@ -4,31 +4,47 @@
 	let form = document.querySelector("#search-form");
 	let linkCelsius = document.querySelector("#celsius");
 	let linkFahrenheit = document.querySelector("#fahrenheit");
+	let daysOfWeek = [
+				"Sunday",
+				"Monday",
+				"Tuesday",
+				"Wednesday",
+				"Thursday",
+				"Friday",
+				"Saturday"
+			];
+	let months = [
+				"January",
+				"February",
+				"March",
+				"April",
+				"May",
+				"June",
+				"July",
+				"August",
+				"September",
+				"October",
+				"November",
+				"December"
+	];
 
 function showData (date){
 	let day = date.getDay();
+	let monthDay = date.getDate();
 	let hour = date.getHours();
 	let minutes = date.getMinutes();
-	let daysOfWeek = [
-			"Sunday",
-			"Monday",
-			"Tuesday",
-			"Wednesday",
-			"Thursday",
-			"Friday",
-			"Saturday"];
+	let month = date.getMonth();
+	
 	if (minutes < 10){
 		minutes = `0${minutes}`;
-	}	
-	if (hour < 10){
+	}else if (hour < 10){
 		hour = `0${hour}`;
-	}
-	if (hour <= 12){
+	}else if (hour <= 12){
 		hour = `${hour}:${minutes}am`;
-	} else {
+	}else {
 		hour = `${hour}:${minutes}pm`;
 	}
-	dayBlock.innerHTML = `${daysOfWeek[day]} ${hour}`;
+	dayBlock.innerHTML = `${months[month]} ${monthDay}, ${daysOfWeek[day]} ${hour}`;
 }
 let apiKey ="1aab98fcee055bc5a48c36f9c5c11358";
 let unit = "metric";	
@@ -47,7 +63,6 @@ function showDefaultCityWeather(){
 	axios.get(defaultApiUrl).then(getWeather);
 }
 
-
 form.addEventListener("submit", (e) => {
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=${unit}`;
 	e.preventDefault();
@@ -56,7 +71,6 @@ form.addEventListener("submit", (e) => {
 		cityName.innerHTML = input.value;
 	}
 });
-
 
 currentBtn.addEventListener("click", e => {
 	e.preventDefault();
@@ -68,9 +82,9 @@ function getForecastDays(time){
 	let date = new Date(time * 1000);
 	let day = date.getDay();
   	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-		return days[day];
-  	
+	return days[day];
 }
+
 function getForecast(response){
 	console.log(response.data);
 	let forecast = response.data.daily;
@@ -80,8 +94,7 @@ function getForecast(response){
 	let forecastHTml = ``;
 	let currentDay = getForecastDays(response.data.daily[1].dt);
 	console.log(currentDay);
-	forecast.forEach((forecastDay, index) =>{
-		let AverageTemp = Math.round((Math.round(forecastDay.temp.max) + Math.round(forecastDay.temp.min))/2);
+	forecast.forEach((forecastDay, index) => {
 		if(index < 5){
 			forecastHtml = forecastHtml +
 			`
@@ -89,8 +102,10 @@ function getForecast(response){
 			<div class="block">
 				<div class="block-title">${getForecastDays(forecastDay.dt)}</div>
 					<img src="./img/${forecastDay.weather[0].icon}.svg" class="weather-icon" width="28" height="28"></img>
-				<div class="block-degrees">${AverageTemp}°</div>
-			</div>
+					
+					<span class="block-degrees reduce-font">${Math.round(forecastDay.temp.max)}°/<span class="block-degrees min">${Math.round(forecastDay.temp.min)}°</span></span>
+					
+					</div>
 			</div>
 			`;
 		}
@@ -98,8 +113,8 @@ function getForecast(response){
 	}); 
 	forecastHtml = forecastHtml + `</div>`;
 	forecastElement.innerHTML = forecastHtml; 
-	
-	forecastHTml = `
+	let tomorrowDegreesValue = Math.round((Math.round(forecast[1].temp.max) + Math.round(forecast[1].temp.min))/2);
+		forecastHTml = `
 			<div class="block-forecast">
 				<img src="./img/${forecast[1].weather[0].icon}.svg" class="fa-cloud-rain" width="130" height="130"></img>
 				<div class="future-content">
@@ -108,21 +123,17 @@ function getForecast(response){
 					<div class="future-deskr">${forecast[1].weather[0].main}</div>
 				</div>
 			</div>`;
-	
-		
-	
 	forecastHTml = forecastHTml + `</div>`;
 	element.innerHTML = forecastHTml;				
 	
 }
-
 
 function getCoord(coordinats){
 	apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinats.lat}&lon=${coordinats.lon}&appid=${apiKey}&units=metric`;
 	//
 	axios.get(apiUrl).then(getForecast);
 	//axios.get(apiUrl).then();
-	
+
 }
 
 function getWeather(response) {
@@ -130,8 +141,8 @@ function getWeather(response) {
 	let tempValue = Math.round(response.data.main.temp);
 	temp.innerHTML = tempValue;
 	let iconElement = document.querySelector(".icon-element");
-	console.log(tempValue);
-	
+	console.log(response.data);
+
 	changeUnit(tempValue);
 	getCoord(response.data.coord);
 	iconElement.setAttribute("src",`./img/${response.data.weather[0].icon}.svg`);
@@ -143,6 +154,7 @@ function getWeather(response) {
 }
 
 function changeUnit (temperature) {
+	
 		linkCelsius.addEventListener("click", (e) => {
 			e.preventDefault();
 			linkCelsius.classList.add("active");
@@ -154,6 +166,7 @@ function changeUnit (temperature) {
 			linkFahrenheit.classList.add("active");
 			linkCelsius.classList.remove("active");
 			temp.innerHTML = Math.round((temperature * 9) / 5 + 32);
+
 		});
 	}
 	
